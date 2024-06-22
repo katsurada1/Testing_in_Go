f5b281f940964b60f01033e00ac96d25991df801
kasturada1 <kazuki1.katsurada@gmail.com>
2024-06-22 16:34:44 +0900

    Add stub handlers for api

--- webapp/cmd/api/routes.go
+++ webapp/cmd/api/routes.go
@@ -12,5 +12,16 @@ func (app *application) routes() http.Handler {
 
 	// register the middleware
 	mux.Use(middleware.Recoverer)
+
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
+
+	mux.Route("/users", func(mux chi.Router) {
+		mux.Get("/", app.allUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/", app.insertUser)
+		mux.Patch("/", app.updateUser)
+	})
 	return mux
 }
