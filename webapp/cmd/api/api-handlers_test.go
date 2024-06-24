f462dc6bccad3969b97d3117bc72e720285f041b
kasturada1 <kazuki1.katsurada@gmail.com>
2024-06-25 01:13:49 +0900

    Add a test for loggin users

--- webapp/cmd/api/api-handlers_test.go
+++ webapp/cmd/api/api-handlers_test.go
@@ -199,7 +199,7 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 		Name:     "__Host-refresh_token",
 		Path:     "/",
 		Value:    tokens.RefreshToken,
-		Expires:  time.Now().Add((refreshTokenExpiry)),
+		Expires:  time.Now().Add(refreshTokenExpiry),
 		MaxAge:   int(refreshTokenExpiry.Seconds()),
 		SameSite: http.SameSiteStrictMode,
 		Domain:   "localhost",
@@ -211,7 +211,7 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 		Name:     "__Host-refresh_token",
 		Path:     "/",
 		Value:    "somebadstring",
-		Expires:  time.Now().Add((refreshTokenExpiry)),
+		Expires:  time.Now().Add(refreshTokenExpiry),
 		MaxAge:   int(refreshTokenExpiry.Seconds()),
 		SameSite: http.SameSiteStrictMode,
 		Domain:   "localhost",
@@ -227,6 +227,7 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 	}{
 		{"valid cookie", true, testCookie, http.StatusOK},
 		{"invalid cookie", true, badCookie, http.StatusBadRequest},
+		{"no cookie", false, nil, http.StatusUnauthorized},
 	}
 
 	for _, e := range tests {
@@ -243,5 +244,30 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 		if rr.Code != e.expectedStatus {
 			t.Errorf("%s: wrong status code returned; expected %d but got %d", e.name, e.expectedStatus, rr.Code)
 		}
+	}
+}
+
+func Test_app_deleteRefreshCookie(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/logout", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(app.deleteRefreshCookie)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusAccepted {
+		t.Errorf("wrong status; expected %d but got %d", http.StatusAccepted, rr.Code)
+	}
+
+	foundCookie := false
+	for _, c := range rr.Result().Cookies() {
+		if c.Name == "__Host-refresh_token" {
+			foundCookie = true
+			if c.Expires.After(time.Now()) {
+				t.Errorf("cookie expiration in future, and should not be: %v", c.Expires.UTC())
+			}
+		}
+	}
+
+	if !foundCookie {
+		t.Error("__Host-refresh_token cookie not found")
 	}
 }
